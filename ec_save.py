02791bcf24990ce752b9432efdff29ed6987cc6f
Stan James <stan@wanderingstan.com>
2019-06-28 15:51:17 -0600

    Store in user's home dir

--- ec_save.py
+++ ec_save.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import os
+import pathlib
 from ec_main import ec_main
 
 """ Simple ec handler that writes stderr to file """
@@ -7,11 +9,19 @@ from ec_main import ec_main
 def save_stderr(data: str):
     """ Save stderr to file """
 
-    filename='test.log'
+    if len(data) == 1:
+        # Probably user typing if only one char
+        return data
+
+    filename = os.path.expanduser('~/.ec/stderr.log')
     with open(filename, 'a') as out:
         out.write(data)
     return data
 
 if __name__ == "__main__":
-    print("Demo EC. Saving stderr to log file.")
+    print("Saving stderr to log file.")
+
+    # Create dir if not exists
+    pathlib.Path(os.path.expanduser('~/.ec')).mkdir(parents=True, exist_ok=True)
+
     ec_main(save_stderr)
