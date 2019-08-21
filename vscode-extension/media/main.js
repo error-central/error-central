500278e5e91e325fdbcd23b9e37c05313371f980
Stan James <stan@wanderingstan.com>
2019-08-21 21:19:11 +0200

    restore contents after users switch tabs

--- vscode-extension/media/main.js
+++ vscode-extension/media/main.js
@@ -1,22 +1,27 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
 (function () {
-    const vscode = acquireVsCodeApi();
-
-    const oldState = vscode.getState();
-
-    const ecDiv = document.getElementById('ec-raw');
-    console.log(oldState);
     console.log("Loaded webview main.js");
-    //vscode.setState({ count: currentCount });
-
+    const vscode = acquireVsCodeApi();
+    const ecDiv = document.getElementById('ec-raw');
+    const oldState = vscode.getState();
+    if (oldState) {
+        // Webview is being restored
+        console.log(oldState);
+        ecDiv.textContent = "Restoring stderr...\n"
+        ecDiv.textContent += oldState.ecDivTextContent;
+    }
+    else {
+        // Webview is launched for first time
+        ecDiv.textContent = "Montioring stderr...\n"
+    }
     // Handle messages sent from the extension to the webview
     window.addEventListener('message', event => {
-        console.info('🔵 event → ', event);
         const message = event.data; // The json data that the extension sent
         switch (message.command) {
             case 'ec':
                 ecDiv.textContent += message.data;
+                vscode.setState({ ecDivTextContent: ecDiv.textContent });
                 break;
         }
     });
