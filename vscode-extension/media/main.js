5dc29607dcf33771de7e86ee046d6d2e990ab7b0
Stan James <stan@wanderingstan.com>
2019-08-23 10:56:22 +0200

    Rudimentary google search working

--- vscode-extension/media/main.js
+++ vscode-extension/media/main.js
@@ -20,7 +20,24 @@
         const message = event.data; // The json data that the extension sent
         switch (message.command) {
             case 'ec':
-                ecDiv.innerHTML += "<div class='errorMessage'>" + message.data + "</div>";
+                // Add element
+                let errorDiv = document.createElement('div');
+                errorDiv.className = 'errorMessage';
+                errorDiv.innerText = message.data;
+                errorDiv.onclick = function () {
+                    console.info('blah');
+                    // TODO: To open web page: https://stackoverflow.com/questions/34205481/how-to-open-browser-from-visual-studio-code-api
+                    document.location="https://google.com" // Won't work
+                };
+
+                let link = document.createElement('a');
+                link.href = `http://google.com/?q=${encodeURIComponent(message.data)}`
+                link.innerText = "Search on Google"
+
+                ecDiv.appendChild(errorDiv);
+                ecDiv.appendChild(link);
+
+                // Remember our total state
                 vscode.setState({ ecDivTextContent: ecDiv.innerHTML });
                 break;
         }
