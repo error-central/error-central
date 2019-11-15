6d43889ea2063aa2a473dffb34f632b3e691c0e1
Stan James <stan@wanderingstan.com>
2019-11-15 12:35:56 -0700

    Create vscode workspace with 3 subprojects.
    Now in theory one can just open this workspace to work on the extension,
    monitoring, or the bash script.

--- ec-bash/ec_server.py
+++ ec-bash/ec_server.py
@@ -14,20 +14,23 @@ class MyServer(BaseHTTPRequestHandler):
 
     #	GET is for clients geting the predi
     def do_GET(self):
-        logfile = os.path.expanduser(os.path.join('~/.ec/', self.path[1:] + '.txt'))
+        logfile = os.path.expanduser(
+            os.path.join('~/.ec/', self.path[1:] + '.txt'))
         with open(logfile) as f:
             read_data = f.read()
         self.send_response(200)
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path[1:], "utf-8"))
+        self.wfile.write(bytes("<p>You accessed path: %s</p>" %
+                               self.path[1:], "utf-8"))
         self.wfile.write(bytes("<pre>%s</pre>" % read_data, "utf-8"))
 
-
     #	POST is for submitting data.
+
     def do_POST(self):
         print("incomming http: ", self.path)
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         self.send_response(200)
+
 
 myServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Started - %s:%s" % (hostName, hostPort))
--- ec-bash/ec_session.sh
+++ ec-bash/ec_session.sh
@@ -1,11 +1,19 @@
-# Save stderr to file
-# must be `source`ed, not `bash`ed.
+# NOTE: this file must be called with `source` not `bash`
 # https://stackoverflow.com/a/52575087/59913
 # https://stackoverflow.com/questions/47302898/redirect-stdout-and-stderr-to-file-permanently-but-keep-printing-them
-echo "💡 ec: Saving stderr to file: ~/.ec/sessions/$$.txt"
-mkdir -p ~/.ec/sessions
+
+# NOTE: $$ is process id of this terminal
+
+# Let user know we're logging
+touch ~/.ec/sessions/$$.txt
+echo "💡 ec: Saving stderr to file: \"~/.ec/sessions/$$.txt"\"
+
+# Run diffenv
+diffenv > ~/.ec/diffenv/$$.yaml
+
+# Log stderr to file
 exec 2> >(tee ~/.ec/sessions/$$.txt 1>&2)
 
-# get pwd of bash session with:
+# Get current pwd of bash session with:
 # https://stackoverflow.com/questions/8327139/working-directory-of-running-process-on-mac-os
 # https://unix.stackexchange.com/questions/94357/find-out-current-working-directory-of-a-running-process
